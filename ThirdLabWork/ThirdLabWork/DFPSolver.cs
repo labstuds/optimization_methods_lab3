d180f49f6b9269f72d3ae64d86573b42e59ea86a
Tontu <2serales96@mail.ru>
2015-11-09 19:21:33 +0300

    Заполнено окно с методом ДФП и сделан каркас будущего кода решателя по этому методу

---
+++ ThirdLabWork/ThirdLabWork/DFPSolver.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ThirdLabWork
+{
+    class DFPSolver
+    {
+        Vector2 basicX;
+        double eps;
+        Vector2 result;
+        public DFPSolver()
+        {
+
+        }
+
+        public void setStartingVariables(Vector2 x, double eps)
+        {
+            basicX = x;
+            this.eps = eps;
+        }
+
+        public Vector2 calculate()
+        {
+            //TO DO place code here
+            return new Vector2(0, 0);
+        }
+
+        public Vector2 returnApproximateSolution(Vector2 x,double eps)
+        {
+            setStartingVariables(x,eps);
+            return calculate();
+        }
+    }
+}
